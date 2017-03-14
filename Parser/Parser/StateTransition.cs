7a838c9035f945fab40d4086338e8a5e30545dba
Carlos <carlozzer@gmail.com>
2017-03-14 22:56:37 +0100

    first commit

---
+++ Parser/Parser/StateTransition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser
+{
+    public class StateTransition
+    {
+        public string Before { get; set; }
+        public string After  { get; set; }
+        public int    Return { get; set; }
+
+        public Action CustomWork;
+    }
+}
