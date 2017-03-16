4ef61ce327c2cdbe54e1d2f3c68f6d4f456d4f23
Carlos <carlozzer@gmail.com>
2017-03-16 22:56:54 +0100

    Integrados tests unitarios

---
+++ Parser/NetParser/StateTransition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetParser
+{
+    public class StateTransition
+    {
+        public string Previous { get; set; }
+        public string Next  { get; set; }
+        public string Pattern { get; set; }
+        public int    Return { get; set; }
+
+        public Action CustomWork;
+
+        public string TokenType { get; set; }
+    }
+}
---
+++ Parser/NetParser/StatesMachine.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NetParser
+{
+    public class StatesMachine
+    {
+        #region CONSTRUCTOR
+
+        public StatesMachine() { 
+            current_state = "INIT";
+        }
+
+        #endregion
+
+        #region TABLE and other locals
+
+        List<StateTransition> TransitionsTable;
+        string current_state;
+        string current_token;
+
+        #endregion
+
+        #region TRANSITIONS
+
+        void EnsureTransitions() {
+            this.TransitionsTable = this.TransitionsTable ?? new List<StateTransition>();
+        }
+
+        public void Add ( StateTransition transition ) {
+
+            this.EnsureTransitions();
+
+            if ( transition != null ) {
+                this.TransitionsTable.Add ( transition );
+            }
+
+
+        }
+
+        public void Add ( 
+            string  before, 
+            string  pattern, 
+            string  after, 
+            int     ret         =0, 
+            Action  CustomAction=null 
+         ) { 
+            StateTransition transition = new StateTransition() { 
+                Before = before,
+                Pattern = pattern,
+                After = after,
+                Return = ret,
+                CustomWork = CustomAction
+            };
+
+            Add( transition );
+        }
+
+        #endregion
+
+        #region REACT
+
+        public int Parse( char c ) {
+            
+            int ret = 0;
+
+            foreach ( StateTransition transition in this.TransitionsTable ) { 
+
+                if ( transition.Previous == current_state ) { 
+
+                    bool pattern_match = Regex.IsMatch( c.ToString() , transition.Pattern );
+
+                    if ( pattern_match ) { 
+
+                        current_token += c.ToString();
+
+                        bool state_change = current_state != transition.Next;
+                        if ( state_change ) {
+                            
+                            // hacer algo con el token
+
+                            ret = transition.Return;
+                            current_state = transition.Next;
+
+                        }
+
+                        if ( transition.CustomWork != null ) { 
+
+                            transition.CustomWork();
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            return ret;
+
+        }
+
+        #endregion
+    }   
+}
